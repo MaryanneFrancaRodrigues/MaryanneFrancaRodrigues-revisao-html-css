e6726479079557c8f312483ee6de112484e021ea
Aluno <aluno@estacio.acad>
2017-08-14 21:41:56 -0300

    vesao2

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+
+var A1 = prompt('Insira o primeiro numero:');
+var B1 = prompt('Insira o segundo numero:');
+
+var A=parseInt(A1);
+var B=parseInt(B1);
+
+var resultado = "A + B é igual: "+ (A+B) + "\n";
+resultado += "A - B é igual: "+ (A-B) + "\n";
+resultado += "A * B é igual: "+ (A*B) + "\n";
+resultado += "A / B é igual: "+ (A/B) + "\n";
+resultado += "A % B é igual: "+ (A%B) + "\n";
+alert(resultado);
